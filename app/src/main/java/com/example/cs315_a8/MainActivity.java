69bb6ae7f753e3e469956d141e355a983bea54f4
Tomo <93736469+Tomokar@users.noreply.github.com>
2022-10-24 21:37:48 -0500

    Commit 1: Fiddling updates

--- app/src/main/java/com/example/cs315_a8/MainActivity.java
+++ app/src/main/java/com/example/cs315_a8/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mButton = this.findViewById(R.id.parseButton);
+        Button mButton = findViewById(R.id.parseButton);
 //        mImageView = this.findViewById(R.id.monsterImageView);
 
         if(mButton != null)
